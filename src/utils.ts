62128d0987efaf4d39771b5e36e5afd96a642412
Lisandra Simonetti <l.simonetti@stragen.fr>
2024-09-04 15:17:23 +0200

    feat: update email table labels to use uppercase and improve header
    - Changed labels for Cc, Bcc, To, Subject, Attachments, and Date
      to uppercase in the generated HTML.
    - Updated the header export to use `dedent` for improved formatting.
    - Renamed `end` to `END` for consistency with other exports.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import type { Readable } from "node:stream";
+import dedent from "ts-dedent";
 import type { MailAddress } from "./interface";
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export async function stream2Buffer(stream: Readable): Promise<any> {
 	return new Promise((resolve, reject) => {
 		const _buf: Uint8Array[] = [];
@@ -12,23 +14,23 @@ export async function stream2Buffer(stream: Readable): Promise<any> {
 }
 
 export const cc = (cc: string) =>
-	`<tr><td class="label">Cc:</td><td>${cc}</td></tr>`;
+	`<tr><td class="label">CC</td><td>${cc}</td></tr>`;
 
 export const bcc = (cci: string) =>
-	`<tr><td class="label">Bcc:</td><td>${cci}</td></tr>`;
+	`<tr><td class="label">BCC</td><td>${cci}</td></tr>`;
 
 export const to = (to: string) =>
-	`<tr><td class="label">To:</td><td>${to}</td></tr>`;
+	`<tr><td class="label">TO</td><td>${to}</td></tr>`;
 
 export const subject = (subject?: string) => {
 	if (!subject) return "";
-	return `<tr><td class="label">Subject:</td><td>${subject}</td></tr>`;
+	return `<tr><td class="label">Subject</td><td>${subject}</td></tr>`;
 };
 
 export const attachments = (attachments?: string) => {
 	if (!attachments)
-		return '<tr><td class="label">Attachments:</td><td>/</td></tr>';
-	return `<tr><td class="label">Attachments:</td><td>${attachments}</td></tr>`;
+		return '<tr><td class="label">Attachments</td><td>/</td></tr>';
+	return `<tr><td class="label">Attachments</td><td>${attachments}</td></tr>`;
 };
 
 export const from = (from?: string) => {
@@ -37,49 +39,53 @@ export const from = (from?: string) => {
 };
 
 export const date = (date: string) =>
-	`<tr><td class="label">Date:</td><td>${date}</td></tr>`;
+	`<tr><td class="label">Date</td><td>${date}</td></tr>`;
 
-export const end = "</table>";
+export const END = "</table><br>";
 
-export const header = `<head>
-    <style>
-        
-        @font-face
-                {font-family:"Cambria Math";
-                panose-1:2 4 5 3 5 4 6 3 2 4;
-                mso-font-charset:0;
-                mso-generic-font-family:roman;
-                mso-font-pitch:variable;
-                mso-font-signature:-536869121 1107305727 33554432 0 415 0;}
-        @font-face
-                {font-family:Aptos;
-                panose-1:2 11 0 4 2 2 2 2 2 4;
-                mso-font-charset:0;
-                mso-generic-font-family:swiss;
-                mso-font-pitch:variable;
-                mso-font-signature:536871559 3 0 0 415 0;}
-        body, html {
-            font-family: Aptos, sans-serif;
-        }
-        .header {
-            font-weight: bold;
-            font-size: 18px;
-        }
-        .underline {
-            border-bottom: 1px solid black;
-            margin: 10px 0;
-        }
-        .email-info {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        .label {
-            font-weight: bold;
-            width: 100px;
-            vertical-align: top;
-            padding-right: 2em;
-        }
-</head>`;
+export const HEADER = dedent(`
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
+			@font-face {
+				font-family:"Cambria Math";
+				panose-1:2 4 5 3 5 4 6 3 2 4;
+				mso-font-charset:0;
+				mso-generic-font-family:roman;
+				mso-font-pitch:variable;
+				mso-font-signature:-536869121 1107305727 33554432 0 415 0;
+			}
+			@font-face {
+				font-family:Aptos;
+				panose-1:2 11 0 4 2 2 2 2 2 4;
+				mso-font-charset:0;
+				mso-generic-font-family:swiss;
+				mso-font-pitch:variable;
+				mso-font-signature:536871559 3 0 0 415 0;
+			}
+			body, html {
+				font-family: Aptos, sans-serif;
+			}
+			.header {
+				font-weight: bold;
+				font-size: 18px;
+			}
+			.underline {
+				border-bottom: 1px solid black;
+				margin: 10px 0;
+				}
+			.email-info {
+				width: 100%;
+				border-collapse: collapse;
+			}
+			.label {
+				font-weight: bold;
+				width: 100px;
+				vertical-align: top;
+				padding-right: 2em;
+			}
+		</style>
+	</head>`);
 export function htmlAddress(addresses?: MailAddress[]) {
 	if (!addresses) return "";
 	const html: string[] = [];
