0608da8c8507a88cc9057f2692f98418b9ad351e
Lisandra Simonetti <l.simonetti@stragen.fr>
2024-10-09 18:23:52 +0200

    test: rewrite

---
+++ tests/eml.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from "bun:test";
+import fs from "node:fs";
+import path from "node:path";
+import { Convert } from "../src";
+import { EmlParser } from "../src";
+const inputs = path.normalize("tests/inputs/EML");
+const output = path.normalize("tests/outputs/EML");
+const input = "momo";
+
+const file = fs.createReadStream(path.join(inputs, `${input}.eml`));
+const emlParser = await EmlParser.init(file, {
+	formatEmailAddress:
+		'<a href="mailto:{{email}}" class="mp_address_email">{{email}}</a>',
+});
+test("EML to HTML", async () => {
+	const html = await emlParser.getAsHtml({
+		excludeHeader: { embeddedAttachments: true },
+	});
+	expect(html).toBeDefined();
+});
+test("List attachments", async () => {
+	const attachments = emlParser.getAttachments();
+	console.log(attachments.length);
+	expect(attachments).toBeDefined();
+});
+test("convert to Buffer", async () => {
+	const html = await emlParser.getAsHtml({
+		excludeHeader: { embeddedAttachments: true },
+	});
+	if (!html) throw "unexpected error";
+	fs.writeFileSync(path.join(output, "html", `${input}.html`), html);
+	const converted = new Convert(html);
+	const buffer = await converted.convertToBuffer();
+	expect(buffer).toBeDefined();
+});
+test("Output pdf", async () => {
+	const html = await emlParser.getAsHtml();
+	if (!html) throw "unexpected error";
+	const converted = new Convert(html);
+	expect(await converted.createPdf(path.join(output, "pdf", `${input}.pdf`)))
+		.pass;
+});
---
+++ tests/msg.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "bun:test";
+import fs from "node:fs";
+import path from "node:path";
+import { Convert } from "../src";
+import { EmlParser } from "../src";
+const inputs = path.normalize("tests/inputs/Msg");
+const output = path.normalize("tests/outputs/Msg");
+const input = "test";
+
+const file = fs.createReadStream(path.join(inputs, `${input}.msg`));
+test("MSG to HTML", async () => {
+	const emlParser = await EmlParser.init(file);
+	const html = await emlParser.getAsHtml();
+	expect(html).toBeDefined();
+});
+test("convert to Buffer", async () => {
+	const emlParser = await EmlParser.init(file);
+	const html = await emlParser.getAsHtml();
+	if (!html) throw "unexpected error";
+	fs.writeFileSync(path.join(output, "html", `${input}.html`), html);
+	const converted = new Convert(html);
+	const buffer = await converted.convertToBuffer();
+	expect(buffer).toBeDefined();
+});
+test("Output pdf", async () => {
+	const emlParser = await EmlParser.init(file);
+	const html = await emlParser.getAsHtml();
+	if (!html) throw "unexpected error";
+	const converted = new Convert(html);
+	expect(await converted.createPdf(path.join(output, "pdf", `${input}.pdf`)))
+		.pass;
+});
